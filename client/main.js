6b626976f9213f9460b292491c14de134b1aa115
SimasDei <simondeiman@gmail.com>
2019-03-13 16:59:02 +0200

    test jsx

--- client/main.js
+++ client/main.js
@@ -1,9 +1,34 @@
 import './main.html';
-import Default, { greetUser, name } from '../imports/utils';
-import { add } from '../imports/math';
-console.log({
-  greetUser: greetUser(),
-  name,
-  math: add(2, 2),
-  Default
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+
+const players = [
+  {
+    _id: 1,
+    name: 'Joe',
+    score: 99
+  },
+  {
+    _id: 2,
+    name: 'Bob',
+    score: -1
+  },
+  {
+    _id: 3,
+    name: 'Susan',
+    score: -12
+  }
+];
+
+Meteor.startup(() => {
+  const name = 'Simas';
+  let jsx = (
+    <div>
+      <h1>Scorify</h1>
+      <p>Lol {name}</p>
+      {[<p>1</p>, <p>2</p>]}
+    </div>
+  );
+  ReactDOM.render(jsx, document.querySelector('#app'));
 });
